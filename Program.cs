ab891231f06a192a91b24db2da4f733c48549f6e
Lethabo Masetloa <khutso.thole@gmail.com>
2022-02-22 18:17:45 +0200

    Program Class

--- Program.cs
+++ Program.cs
@@ -20,7 +20,8 @@ namespace Robot
         private static LoggerConfiguration loggerConfiguration = null;
         public static void Main(string[] args)
         {
-            var path = @"C:\Users\Lethaboma\source\repos\Robot";
+            var path = Directory.GetCurrentDirectory();
+          
             config = new ConfigurationBuilder()
                 .SetBasePath(path)
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
@@ -31,7 +32,7 @@ namespace Robot
             loggerConfiguration = new LoggerConfiguration()
                 .WriteTo.File(
                      outputTemplate:"[{Timestamp:yyyy-MM-dd HH:mm:ss:fff} {Level:u3}] {Message:1}{NewLine}{Exception}",
-                     path: Directory.GetCurrentDirectory(),
+                     path: Directory.GetCurrentDirectory() + @"\Logs\Log_.txt",
                      fileSizeLimitBytes:5242880,
                      flushToDiskInterval:TimeSpan.FromSeconds(1),
                      rollOnFileSizeLimit:true,
@@ -40,7 +41,7 @@ namespace Robot
                 );
 
             Log.Logger = loggerConfiguration.CreateLogger();
-         
+            Log.Information("Starting the API");
 
             CreateHostBuilder(args).Build().Run();
         }
